b94825a42b7ae53a7bdd414878ba7b83c977c2a7
Luke Edwards <luke.edwards05@gmail.com>
2016-06-21 05:42:29 -0800

    add tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,25 @@
 import test from 'ava';
 import fn from '../';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+const id = str => document.getElementById(str);
+
+const elms = {};
+const body = document.body;
+
+['a1', 'b1', 'b2', 'c1'].map(e => elms[e] = id(e));
+
+test('empty node, finds sibling', t => {
+	t.is( fn(elms.b1), elms.b2 );
+});
+
+test('container node, finds child', t => {
+	t.is( fn(elms.a1), elms.b1 );
+});
+
+test('last child, exits container', t => {
+	t.is( fn(elms.b2), elms.c1 );
+});
+
+test('last child, prevent container exit', t => {
+	t.is( fn(elms.b2, elms.a1), null );
 });
--- test/setup.js
+++ test/setup.js
@@ -1,1 +1,11 @@
-global.document = require('jsdom').jsdom('<body></body>');
+var html = [
+	'<body>',
+		'<div id="a1">',
+			'<div id="b1"></div>',
+			'<div id="b2"></div>',
+		'</div>',
+		'<div id="c1"></div>',
+	'</body>'
+].join('');
+
+global.document = require('jsdom').jsdom(html);
